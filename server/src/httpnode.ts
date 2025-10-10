86ec7a323598710afb10f81bd23a05285ecfdcc1
Rithish <rithish9806@gmail.com>
2025-10-11 02:44:31 +0530

    feat:basic httpnode

--- server/src/httpnode.ts
+++ server/src/httpnode.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import axios from "axios";
 
 async function handleHttp(req: Request, res: Response) {
-  const inpdata = req.body;
+  const { apiUrl, payload } = req.body;
   try {
-    const response = await axios.post("APIURL", inpdata,{
+    const response = await axios.post(apiUrl, payload, {
       headers: { "Content-Type": "application/json" },
     });
     res.json(response.data);
